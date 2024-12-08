d073f793b8c0a1ac4d28aea470b817b9a2d9c09b
Dylan Throckmorton <115873644+officialthrocky@users.noreply.github.com>
2024-12-08 00:15:59 -0600

    Added Guards and updated to builtin.h

--- shell.h
+++ shell.h
@@ -1,10 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define MAX_PATH_LENGTH 1024
+#define MAX_HISTORY 20
+
 void prompt();
 void execute_command(char **args);
 int handle_builtin_commands(char **args);
 int handle_history_command(char **args);
 void add_to_history(const char *command);
+void clear_history();
 
 #endif
