6e542de4faa7e8f633f96e84b93ae97e34c35cdf
Dylan Throckmorton <115873644+officialthrocky@users.noreply.github.com>
2024-12-08 00:18:05 -0600

    Added changes to history.c

--- history.c
+++ history.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define MAX_HISTORY 20
+#include "shell.h"
 
 char *history[MAX_HISTORY];
 int history_count = 0;
@@ -12,7 +11,7 @@ void add_to_history(const char *command) {
         history[history_count] = strdup(command);
         history_count++;
     } else {
-        free(history[0]); // Free the oldest history
+        free(history[0]);
         for (int i = 1; i < MAX_HISTORY; i++) {
             history[i - 1] = history[i];
         }
@@ -44,21 +43,20 @@ int handle_history_command(char **args) {
             int cmd_index = atoi(args[2]) - 1;
             if (cmd_index >= 0 && cmd_index < history_count) {
                 printf("Executing: %s\n", history[cmd_index]);
-                // Execute the historical command
-                char *cmd = history[cmd_index];
-                char *args[100];
-                char *token = strtok(cmd, " \n");
+                char *cmd = strdup(history[cmd_index]);
+                char *cmd_args[100];
                 int i = 0;
+                char *token = strtok(cmd, " \n");
                 while (token != NULL) {
-                    args[i++] = token;
+                    cmd_args[i++] = token;
                     token = strtok(NULL, " \n");
                 }
-                args[i] = NULL;
-                execute_command(args); // Reusing the execute_command function from main.c
+                cmd_args[i] = NULL;
+                execute_command(cmd_args);
+                free(cmd);
             }
         }
         return 1;
     }
-
     return 0;
 }
