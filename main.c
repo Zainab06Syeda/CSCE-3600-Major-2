75cc4db46007e7762d59cddbf1e67f9022dd0ac0
Dylan Throckmorton <115873644+officialthrocky@users.noreply.github.com>
2024-12-08 00:16:25 -0600

    made main.c accept builtin.c changes

--- main.c
+++ main.c
@@ -3,9 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-
-#define MAX_HISTORY 20
+#include "shell.h"
 
 void prompt() {
     printf("my_shell> ");
@@ -14,13 +12,11 @@ void prompt() {
 void execute_command(char **args) {
     pid_t pid = fork();
     if (pid == 0) {
-        // Child process
         if (execvp(args[0], args) == -1) {
             perror("Error executing command");
         }
         exit(EXIT_FAILURE);
     } else if (pid > 0) {
-        // Parent process
         wait(NULL);
     } else {
         perror("Fork failed");
@@ -30,19 +26,18 @@ void execute_command(char **args) {
 int main(int argc, char *argv[]) {
     char *line = NULL;
     size_t len = 0;
-    ssize_t nread;
     char *args[100];
     
     while (1) {
         prompt();
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1) {
+        if (getline(&line, &len, stdin) == -1) {
             perror("getline");
             break;
         }
 
-        // Tokenize the input
+        add_to_history(line);
+
         int i = 0;
         char *token = strtok(line, " \n");
         while (token != NULL) {
@@ -51,18 +46,16 @@ int main(int argc, char *argv[]) {
         }
         args[i] = NULL;
 
-        if (args[0] == NULL) {
-            continue; // Empty command
-        }
+        if (args[0] == NULL) continue;
 
-        // Implement built-in commands here
-        if (strcmp(args[0], "exit") == 0) {
-            break;
+        if (handle_builtin_commands(args) || handle_history_command(args)) {
+            continue;
         }
 
         execute_command(args);
     }
 
+    clear_history();
     free(line);
     return 0;
 }
